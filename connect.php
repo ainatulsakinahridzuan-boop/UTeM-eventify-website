ee44a96126df6b75166f17f331b0490178df896e
natashasabri-cmd <natashasabri1234@gmail.com>
2026-06-19 22:11:22 +0800

    Added connect.php

---
+++ connect.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost:3307";
+$username = "eventify_user";
+$password = "eventify2026";
+$dbname = "utem_eventify";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+//echo "Connected successfully";
